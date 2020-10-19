78cb60c43d20c15138e03007b3fa47f98262f53f
Sajid1500 <sajidhuq.chowdhury@gmail.com>
2020-10-19 20:46:04 +0600

    hhg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import useFetchJobs from "./useFetchJobs";
+import { Container } from "react-bootstrap";
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [params, setParams] = useState();
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error } = useFetchJobs(params, page);
+  return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try refreshing</h1>}
+      {jobs.map((job) => {})}
+      <h1>{jobs.length}</h1>
+    </Container>
+  );
 }
 
 export default App;
